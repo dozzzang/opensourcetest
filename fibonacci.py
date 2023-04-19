8361d898823810bebc4e7d6130af8d8ec8f8721d
Dozzang <74997120+dozzzang@users.noreply.github.com>
2023-04-19 12:09:17 +0900

    Update fibonacci.py
    contain Docstring ver.Sphinx

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,12 @@
 def fib(n):
+    """Returns list's last port
+       This is fibonacci's function.
+       
+       :param n: n은 3이상이어야한다. 3 이상의 n을 입력
+       :type n: int, optional
+       :return: list of returned : last fibonacci's number
+       :rtyp: int
+       """
     fib_list = [0,1,1];
     for i in range(3,n+1):
       new_fib = fib_list[-1] + fib_list[-2]
